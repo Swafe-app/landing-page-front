f34b889b10920ef72f7ff8e638f1ff2e00349ffc
Fabien Fiette <fabienfiette@hotmail.fr>
2023-05-22 11:33:59 +0200

    fix: Update css of Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import './Header.scss';
 
 function Header() {
   return (
-    <header className='header sectionApp'>
+    <header className='header'>
       <div className='headerContent'>
         <h1 className='mainTitle'>Il n’a jamais été aussi simple d’aller d'un point A à un point B en toute sécurité</h1>
         <img src='https://placehold.co/600' alt='Placeholder' />
