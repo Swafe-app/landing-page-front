370d5f4356139c213f86e815e3309b1f392e966c
Fabien Fiette <fabienfiette@hotmail.fr>
2023-05-22 16:35:11 +0200

    feat: Implement a scroll section

--- src/components/SectionList/SectionList.js
+++ src/components/SectionList/SectionList.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './SectionList.scss';
 
 const sectionListData = [
@@ -29,11 +30,56 @@ const sectionListData = [
 ];
 
 function SectionList() {
+  const [currentSectionIndex, setCurrentSectionIndex] = useState(0);
+
+  const scrollToSection = (sectionIndex) => {
+    const section = document.querySelector(`#section-${sectionIndex}`);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const handleScroll = (event) => {
+    if (event.deltaY > 0) {
+      // Scrolling down
+      setCurrentSectionIndex((index) => Math.min(index + 1, sectionListData.length));
+    } else {
+      // Scrolling up
+      setCurrentSectionIndex((index) => Math.max(index - 1, 0));
+    }
+  };
+
+  const handleArrowKey = (event) => {
+    if (event.keyCode === 40 || event.keyCode === 32 || event.keyCode === 39) {
+      setCurrentSectionIndex((index) => Math.min(index + 1, sectionListData.length));
+    } else if (event.keyCode === 38 || event.keyCode === 37) {
+      setCurrentSectionIndex((index) => Math.max(index - 1, 0));
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('wheel', handleScroll);
+    window.addEventListener('keydown', handleArrowKey);
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+      window.removeEventListener('keydown', handleArrowKey);
+    };
+  }, []);
+
+  useEffect(() => {
+    scrollToSection(currentSectionIndex);
+  }, [currentSectionIndex]);
+
   return (
     <>
       {
         sectionListData.map((blockData, index) => (
-          <section key={index} className='sectionApp' style={{ color: blockData.color }}>
+          <section
+            key={index}
+            id={`section-${index + 1}`}
+            className='sectionApp'
+            style={{ color: blockData.color }}
+          >
             <h2 className='sectionTitle'>{blockData.title}</h2>
             <span className='subTitle'>{blockData.subTitle}</span>
           </section>
