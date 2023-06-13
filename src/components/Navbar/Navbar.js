802bbcbe46bfeef05bb8cdcfca349693cfb7fbd6
Fabien Fiette <fabienfiette@hotmail.fr>
2023-06-13 19:30:07 +0200

    feat: Navbar hide on forms view

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,10 +3,23 @@ import { ReactComponent as FacebookIcon } from '../../assets/FacebookIcon.svg';
 import { ReactComponent as InstagramIcon } from '../../assets/InstagramIcon.svg';
 import { ReactComponent as LinkedinIcon } from '../../assets/LinkedinIcon.svg';
 import SwafeLogo from '../../assets/SwafeLogo.png';
+import { useScrollSection } from '../../hooks/ScrollSection/ScrollSection';
+import { useEffect, useState } from 'react';
 
 function Navbar() {
+  const { currentSectionIndex } = useScrollSection();
+  const [displayNavbar, setDisplayNavbar] = useState(true);
+
+  useEffect(() => {
+    if (currentSectionIndex === 6) {
+      setDisplayNavbar(false);
+    } else {
+      setDisplayNavbar(true);
+    };
+  }, [currentSectionIndex])
+
   return (
-    <nav className='navbar'>
+    <nav className={`navbar ${displayNavbar ? 'visible' : 'none'}`}>
       <div className='logoContainer'>
         <img src={SwafeLogo} alt='Swafe logo' />
       </div>
